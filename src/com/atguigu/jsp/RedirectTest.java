10c460f88767d591471411a602874d330dccb01d
HDXin <735513870@qq.com>
2017-04-21 23:15:39 +0800

    jsp

---
+++ src/com/atguigu/jsp/RedirectTest.java
@@ -0,0 +1,27 @@
+package com.atguigu.jsp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RedirectTest
+ */
+public class RedirectTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("redirectTest...");
+		String path = "redirectPage";
+		
+		response.sendRedirect("http://www.baidu.com");
+		
+	}
+
+}
